5ac95898ce660ce66c75a20dfed5311b482cd62f
= <=>
2021-04-28 21:42:19 +0700

    Perbaikan Praktikum 2

--- todo-api/app/Exceptions/Handler.php
+++ todo-api/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
